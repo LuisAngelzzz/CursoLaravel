db2cdac1fb97756773be7b0d432416014143cbda
Luis Ángel Domínguez Gutiérrez <angeelgamer1@gmail.com>
2024-01-24 13:29:50 -0600

    5. Grupo de rutas https://codersfree.com/courses-status/aprende-laravel-desde-cero/grupo-de-rutas

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,10 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/', HomeController::class);
 
-route::get('cursos', [CursoController::class, 'index']);
+Route::controller(CursoController::class)->group(function(){
 
+route::get('cursos','index');
+route::get('cursos/create', 'create');
+route::get('cursos/{curso}','show');
 
-route::get('cursos/create', [CursoController::class, 'create']);
-
-route::get('cursos/{curso}', [CursoController::class, 'show']);
-
-
-//     route::get('cursos/{curso}/{categoria}', function($curso, $categoria = null){
-
-//         if($categoria){
-//             return "bienvenido al curso: $curso, de la categoria $categoria";
-//         }else{
-//             return "bienvenido al curso: $curso";
-//         }
-        
-//  });
+});
