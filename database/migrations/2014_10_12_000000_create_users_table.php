95c047fde1e0c03ca255fff3aa4cf572815a494d
Luis Ángel Domínguez Gutiérrez <angeelgamer1@gmail.com>
2024-01-24 14:59:23 -0600

    8. Introducción a las migraciones https://codersfree.com/courses-status/aprende-laravel-desde-cero/introduccion-a-las-migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id(); //integer unsigned increment
+            $table->string('name');//varchar
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();//created_at updated_at
         });
     }
 
