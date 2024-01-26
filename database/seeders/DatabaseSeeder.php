af245094206c57d3e14ee80bc9d70975e00c68bd
Luis Ángel Domínguez Gutiérrez <angeelgamer1@gmail.com>
2024-01-25 23:26:48 -0600

    12. Seeders en Laravel ¿Qué son y para que sirven?
    https://codersfree.com/courses-status/aprende-laravel-desde-cero/seeders-en-laravel-que-son-y-para-que-sirven

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,12 @@ class DatabaseSeeder extends Seeder
     
     public function run(): void
     {
+<<<<<<< HEAD
         User::factory(10)->create();
         curso::factory(50)->create();
             
+=======
+          $this->call(CursoSeeder::class);
+>>>>>>> ef304eb (12. Seeders en Laravel ¿Qué son y para que sirven?)
     }
 }
